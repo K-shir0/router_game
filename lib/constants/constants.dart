d22273d5939b42a6dfefd7b0b83b5fcc081c2c6a
K-shir0 <kakishiro2190394@gmail.com>
2022-08-02 10:05:27 +0900

    feat: 色を共通になるよう変更

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class GameColors {
+  GameColors._();
+
+  static const Color backgroundColor = Color(0x00F2F2F2);
+}
---
+++ lib/two_node_one_router.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+import 'package:router_game_f/constants/constants.dart';
+
+class TwoNodeOneRouter extends FlameGame {
+  @override
+  Color backgroundColor() => GameColors.backgroundColor;
+}
