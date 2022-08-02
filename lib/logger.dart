c290f6a6362e45784d4b173beb4d15b6672114b8
K-shir0 <kakishiro2190394@gmail.com>
2022-08-02 11:53:47 +0900

    feat: ルータを実装

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,24 +1,9 @@
-import 'dart:io';
-
-import 'package:logger/logger.dart' as logger;
+import 'dart:developer' as developer;
 
 class Logger {
   Logger._();
 
-  static final _logger = logger.Logger(
-    // iOS だとコンソールの色がうまく反映されない。
-    printer: logger.PrettyPrinter(colors: !Platform.isIOS),
-  );
-
-  static void info(dynamic message) {
-    _logger.i(message);
-  }
-
-  static void warning(dynamic message) {
-    _logger.w(message);
-  }
-
-  static void error(dynamic error, [StackTrace? stack]) {
-    _logger.e('Error!', error, stack);
+  static void debug(String message) {
+    developer.log(message, name: 'debug');
   }
 }
